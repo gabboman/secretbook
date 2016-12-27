d774ee2f44df96a729dbb242ceda6cf9508ad587
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-12-28 00:50:10 +0100

    lets get started, shall we?

--- models/user.js
+++ models/user.js
@@ -2,14 +2,19 @@
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    username: DataTypes.STRING
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    wishlist: DataTypes.STRING//Libros en los que puede estar interesado
   }, {
     classMethods: {
       associate: function(models) {
         User.hasMany(models.Task)
       }
     }
-  });
+  }
+  );
 
   return User;
 };
