f29f6c781a37863e6357f5a9ae558de9d8a68c61
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-12-28 01:26:26 +0100

    iniciando pagina de ejemplo para login

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users  = require('./routes/users');
+//añadir primero aqui
+var login = require ('./routes/login');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+//AÑADIR SEGUNDO AQUI
+app.use('/login', login)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
