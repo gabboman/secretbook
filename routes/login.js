2005d45d9f1f3e8180640827a4f25a9d283a485c
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2016-12-30 01:01:06 +0100

    vamos a probar con migraciones

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,50 @@
 var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
+//cosas de login con bcrypt:
 
+var bcrypt = require('bcrypt');
+const saltRounds = 10;//Mas cpu, mas seguridad. we should choose
+
+
+//fin cosas login
 router.get('/', function(req, res) {
-  models.User.findAll({
-    include: [ models.Task ]
-  }).then(function(users) {
-    res.render('index', {
-      title: 'Sequelize: Express Example',
-      users: users
+    res.render('login', {
+      
+      exito: false
     });
-  });
+});
+
+router.post('/login', function(req, res) {
+
+
+usuario=req.body.username
+password= req.body.password
+models.User.findOne({where:{username:{like:usuario}}}).then(function(fullUser){
+//si el usuario no está registrado
+if(fullUser == undefined){
+  res.render('login', {
+      
+      exito: false
+    });
+}else{
+console.log(fullUser)
+bcrypt.compare(password, fullUser.password, function(err, isPasswordCorrect) {
+
+   res.render('login', {
+      
+      exito: isPasswordCorrect
+    });
+});
+
+}
+
+
+
+})
+
+  
+
 });
 
 module.exports = router;
